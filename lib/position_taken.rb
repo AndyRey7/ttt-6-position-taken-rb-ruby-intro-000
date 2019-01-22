ad2828a85fc17f4e851bbeddbd624b87d176a49c
andy reyes <11710reyes@gmail.com>
2019-01-22 16:46:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, board_index)
   if board[board_index] == " " || ""
-    return FALSE 
+    return FALSE
+  else 
+    return TRUE
   end
 end
